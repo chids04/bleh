2f518191a91f86d667de07de6192dcf07a2f8ea1
chids <guyepic39@gmail.com>
2025-07-20 17:48:27 +0100

    adding astro config

---
+++ src/components/Settings.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+import { Button } from "@/components/ui/button"
+
+export default function HomePage() {
+
+    const handleClick = async () => {
+
+    }
+
+    return (
+        <div>
+            <Button onClick={handleClick}>scan directory</Button>
+        </div>
+    )
+}
